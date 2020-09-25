f76b4cd74d813ef42573eb5c1e9faee599121acd
pkkarn <pk2psp@gmail.com>
2020-09-25 11:52:38 +0530

    ModalViewSet and GnericViewSet

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from api_basic import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('gen_viewset', views.ArticleViewSet, basename='article')
+router.register('mod_viewset', views.ArticleModalViewSet, basename='article_modal')
+
 urlpatterns = [
+    path('viewset/', include(router.urls)),
     path('function/', views.article_list, name='function_api'),
     path('function/<int:pk>/', views.article_detail, name='function_api_pk'),
     path('class/', views.ArticleAPIView.as_view(), name='class_api'),
--- api_basic/views.py
+++ api_basic/views.py
@@ -12,9 +12,30 @@ from rest_framework import generics
 from rest_framework import mixins
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
 
 # GET, PUT(UPDATE), POST(CREATE), DELETE
+
+
+# ViewSet : - [viewsets  - Easiest
+class ArticleViewSet(viewsets.GenericViewSet,
+                     mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.DestroyModelMixin):
+
+    serializer_class = ArticleSerializers
+    queryset = Article.objects.all()
+
+
+#Easiest way  - But Use
+class ArticleModalViewSet(viewsets.ModelViewSet,):
+
+    serializer_class = ArticleSerializers
+    queryset = Article.objects.all()
+
 
 # API - Function Based View  [api_view, modelSerializers, Response, status]
 
